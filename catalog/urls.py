71a234155c366f67f3af0343809229b4ccaa4f5a
MarquisWhale <Marquis.404@outlook.com>
2023-12-30 13:03:14 +0300

    Project version 1.01

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
-from django.urls import include, path
-from django.views.generic import RedirectView
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+
 ]
